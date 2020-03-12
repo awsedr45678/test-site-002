04034c182124011786d6f88827a7bbed5b18947d
awsedr45678 <59809816+awsedr45678@users.noreply.github.com>
2020-03-12 15:28:40 +0700

    Create main.js

---
+++ main.js
@@ -0,0 +1,54 @@
+
+///////タブ切り替え
+
+document.addEventListener('DOMContentLoaded', function(){
+    // タブに対してクリックイベントを適用
+    const tabs = document.getElementsByClassName('tab');
+    for(let i = 0; i < tabs.length; i++) {
+        tabs[i].addEventListener('click', tabSwitch);
+    }
+
+    // タブをクリックすると実行する関数
+    function tabSwitch(){
+        // タブのclassの値を変更
+        document.getElementsByClassName('is-active')[0].classList.remove('is-active');
+        this.classList.add('is-active');
+        // コンテンツのclassの値を変更
+        document.getElementsByClassName('is-show')[0].classList.remove('is-show');
+        const arrayTabs = Array.prototype.slice.call(tabs);
+        const index = arrayTabs.indexOf(this);
+        document.getElementsByClassName('panel')[index].classList.add('is-show');
+    };
+});
+
+$(function(){
+    $('.js-modal-open').on('click',function(){
+        $('.js-modal').fadeIn();
+        return false;
+    });
+    $('.js-modal-close').on('click',function(){
+        $('.js-modal').fadeOut();
+        return false;
+    });
+});
+
+
+
+$(window).on('load', function(){
+    // ローディング画面
+	$('.loading').fadeOut();	
+});
+
+
+$(function(){
+    // アコーディオン部分
+    //.accordion_oneの中の.accordion_headerがクリックされたら
+    $('.s_02 .accordion_one .accordion_header').click(function(){
+      //クリックされた.accordion_oneの中の.accordion_headerに隣接する.accordion_innerが開いたり閉じたりする。
+      $(this).next('.accordion_inner').slideToggle();
+      $(this).toggleClass("open");
+      //クリックされた.accordion_oneの中の.accordion_header以外の.accordion_oneの中の.accordion_headerに隣接する.accordion_oneの中の.accordion_innerを閉じる
+      $('.s_02 .accordion_one .accordion_header').not($(this)).next('.accordion_one .accordion_inner').slideUp();
+      $('.s_02 .accordion_one .accordion_header').not($(this)).removeClass("open");
+    });
+  });
